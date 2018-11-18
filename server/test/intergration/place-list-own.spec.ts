cf700b0cbcc971ccd9df4d02ec5175125e616260
Mikhail Pontus <m.pontus@gmail.com>
2018-11-18 20:23:16 +0300

    Test pagination for own places

--- server/test/intergration/place-list-own.spec.ts
+++ server/test/intergration/place-list-own.spec.ts
@@ -30,6 +30,83 @@ describe('List Own Places', () => {
 
       expect(response.body).toMatchSnapshot();
     });
+
+    describe('when limit is provided', () => {
+      it('should succeed', async () => {
+        const response = await supertest(nestApp.getHttpServer())
+          .get(`/places/own?take=2`)
+          .set('Authorization', `Bearer ${authSeed.accessToken}`)
+          .expect(200);
+
+        expect(response.body).toMatchSnapshot();
+      });
+    });
+
+    describe('when offset is provided', () => {
+      it('should succeed', async () => {
+        const response = await supertest(nestApp.getHttpServer())
+          .get(`/places/own?skip=3`)
+          .set('Authorization', `Bearer ${authSeed.accessToken}`)
+          .expect(200);
+
+        expect(response.body).toMatchSnapshot();
+      });
+    });
+
+    describe('when limit is not a number', () => {
+      it('should fail', async () => {
+        const response = await supertest(nestApp.getHttpServer())
+          .get(`/places/own?take=foo`)
+          .set('Authorization', `Bearer ${authSeed.accessToken}`)
+          .expect(400);
+
+        expect(response.body).toMatchSnapshot();
+      });
+    });
+
+    describe('when limit is negative', () => {
+      it('should fail', async () => {
+        const response = await supertest(nestApp.getHttpServer())
+          .get(`/places/own?take=-1`)
+          .set('Authorization', `Bearer ${authSeed.accessToken}`)
+          .expect(400);
+
+        expect(response.body).toMatchSnapshot();
+      });
+    });
+
+    describe('when limit is too large', () => {
+      it('should fail', async () => {
+        const response = await supertest(nestApp.getHttpServer())
+          .get(`/places/own?take=9000`)
+          .set('Authorization', `Bearer ${authSeed.accessToken}`)
+          .expect(400);
+
+        expect(response.body).toMatchSnapshot();
+      });
+    });
+
+    describe('when offset is not a number', () => {
+      it('should fail', async () => {
+        const response = await supertest(nestApp.getHttpServer())
+          .get(`/places/own?skip=foo`)
+          .set('Authorization', `Bearer ${authSeed.accessToken}`)
+          .expect(400);
+
+        expect(response.body).toMatchSnapshot();
+      });
+    });
+
+    describe('when offset is negative', () => {
+      it('should fail', async () => {
+        const response = await supertest(nestApp.getHttpServer())
+          .get(`/places/own?skip=-1`)
+          .set('Authorization', `Bearer ${authSeed.accessToken}`)
+          .expect(400);
+
+        expect(response.body).toMatchSnapshot();
+      });
+    });
   });
 
   describe('when user is not an owner', () => {
