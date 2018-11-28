0e25ae523da7d758becf096ab6e483002384cb7b
Mikhail Pontus <m.pontus@gmail.com>
2018-11-28 21:35:40 +0300

    Handle failure to load list / details

--- client/src/reducers/notificationReducer.ts
+++ client/src/reducers/notificationReducer.ts
@@ -3,6 +3,7 @@ import { getType } from "typesafe-actions";
 import { Action } from "../actions";
 import * as authActions from "../actions/authActions";
 import * as notificationActions from "../actions/notificationActions";
+import { loadPlace } from "../actions/placeDetailsActions";
 import * as placeListActions from "../actions/placeListActions";
 import * as reviewListActions from "../actions/reviewListActions";
 import * as userListActions from "../actions/userListActions";
@@ -121,6 +122,12 @@ export const notificationReducer: Reducer<State, Action> = (
     case getType(reviewListActions.updateReview.failure):
     case getType(reviewListActions.deleteReview.failure):
       return addError(state, action.payload.error.message);
+
+    case getType(loadPlace.failure):
+    case getType(placeListActions.loadPlaces.failure):
+    case getType(reviewListActions.loadReviews.failure):
+    case getType(userListActions.loadUsers.failure):
+      return addError(state, "An error occured. Please refresh the page");
 
     case getType(notificationActions.dismissNotification):
       return dismiss(state);
