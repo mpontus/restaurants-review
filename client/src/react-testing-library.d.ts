d80bc81c0f9cc7e677a4b7849c2096c19777c3f7
Mikhail Pontus <m.pontus@gmail.com>
2018-11-28 11:14:30 +0300

    Install dependencies for testing

---
+++ client/src/react-testing-library.d.ts
@@ -0,0 +1,39 @@
+/**
+ * Type declaration for react-testing-library
+ *
+ * Includes flushEffects missing from original typings.
+ */
+declare module "react-testing-library" {
+  import { getQueriesForElement } from "dom-testing-library";
+
+  export * from "dom-testing-library";
+
+  type GetsAndQueries = ReturnType<typeof getQueriesForElement>;
+
+  export interface RenderResult extends GetsAndQueries {
+    container: HTMLElement;
+    baseElement: HTMLElement;
+    debug: (baseElement?: HTMLElement | DocumentFragment) => void;
+    rerender: (ui: React.ReactElement<any>) => void;
+    unmount: () => boolean;
+    asFragment: () => DocumentFragment;
+  }
+
+  /**
+   * Render into a container which is appended to document.body. It should be used with cleanup.
+   */
+  export function render(
+    ui: React.ReactElement<any>,
+    options?: { container: HTMLElement; baseElement?: HTMLElement }
+  ): RenderResult;
+
+  /**
+   * Unmounts React trees that were mounted with render.
+   */
+  export function cleanup(): void;
+
+  /**
+   * Flush pending effects
+   */
+  export function flushEffects(): void;
+}
